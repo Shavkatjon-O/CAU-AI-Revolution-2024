7d61168f76b90590e856072b9c70c238a20398e1
CoderB7 <iutstudent2022@gmail.com>
2024-11-08 18:37:07 +0500

    feat: Initial changes 2

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from .schema import swagger_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path('rosetta/', include('rosetta.urls')),
 ]
+
+urlpatterns += [
+    path("api/users/", include('apps.users.urls')),
+]
+
+urlpatterns += swagger_urlpatterns
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
