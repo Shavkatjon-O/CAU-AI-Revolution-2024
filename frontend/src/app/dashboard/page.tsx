dbb7af57860506d6d60c6d5f19e025689f96c095
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-12 15:28:50 +0500

    fix

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,14 +1,6 @@
-import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
-
 const Page = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <Button asChild>
-        <Link href="/sign-up">Sign Up</Link>
-      </Button>
-    </div>
+    <div>Home</div>
   )
 }
 export default Page;
