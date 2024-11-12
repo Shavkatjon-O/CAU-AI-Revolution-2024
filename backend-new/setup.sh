d8fbba770f4ad6c4a97d0d30c6979dc630cf562f
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-12 15:28:50 +0500

    feat: new backend setup

---
+++ backend-new/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check_postgres_connection() {
+    while ! pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
+        echo "Waiting for PostgreSQL to become available..."
+        sleep 1
+    done   
+    echo "PostgreSQL is available."
+}
+
+check_postgres_connection
+
+python manage.py migrate
+python manage.py collectstatic --no-input
+
+exec "$@"
