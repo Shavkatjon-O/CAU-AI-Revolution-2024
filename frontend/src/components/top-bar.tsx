2580fdf624b2409219da6a61632c897da8fef6bf
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-12 15:28:50 +0500

    fix

--- frontend/src/components/top-bar.tsx
+++ frontend/src/components/top-bar.tsx
@@ -1,7 +1,7 @@
 
 const TopBar = () => {
 	return (
-		<div className="fixed top-0 right-0 left-0 h-20 flex justify-center items-center border-b border-b-slate-300 rounded-3xl">
+		<div className="fixed top-0 right-0 left-0 h-20 flex justify-center items-center border-b border-b-slate-300 rounded-b-3xl bg-white">
 			<span className="text-xl text-custom font-semibold">SafeBite</span>
 		</div>
 	)
