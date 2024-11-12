5c56d9990f1b609ee5aa9b926ae421b296ae3d16
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-12 15:28:50 +0500

    feat: sign out

--- frontend-new/src/app/sign-out/page.tsx
+++ frontend-new/src/app/sign-out/page.tsx
@@ -1,6 +1,26 @@
+"use client"
+
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { Loader2 } from "lucide-react";
+
 const Page = () => {
-    return (
-      <div>Sign Out</div>
-    )
-  }
-  export default Page;
+  const router = useRouter();
+
+  useEffect(() => {
+    Cookies.remove("access_token");
+    Cookies.remove("refresh_token");
+
+    router.push("/sign-in");
+  }, [router]);
+
+  return (
+    <div className="size-full flex justify-center items-center">
+      <Loader2 className="animate-spin text-custom" />
+    </div>
+  );
+}
+
+export default Page;
