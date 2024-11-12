1789bb80fae35f8559d0b62b38d198f07e6ffdc7
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-12 15:28:50 +0500

    feat: chat page

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,31 +1,37 @@
 "use client";
 
-import { useState } from "react";
-// import { Input } from "@/components/ui/input";
-import {
-  SendHorizontal,
-} from "lucide-react";
+import { useState, useRef, useEffect } from "react";
+import { SendHorizontal, Check } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 interface Message {
   text: string;
   sender: string;
+  timestamp: string;
 }
 
 const ChatPage = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState("");
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
   const handleSendMessage = () => {
     if (input.trim()) {
-      setMessages([...messages, { text: input, sender: "User" }]);
+      const timestamp = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      setMessages([...messages, { text: input, sender: "User", timestamp }]);
       setInput("");
     }
   };
 
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
   return (
     <div className="flex flex-col h-full">
-      <div className="flex-grow p-4 overflow-y-auto space-y-4">
+      <div className="flex-grow p-4 overflow-y-auto gap-y-4 flex flex-col-reverse">
         {messages.map((message, index) => (
           <div
             key={index}
@@ -33,15 +39,23 @@ const ChatPage = () => {
           >
             <div
               className={`max-w-xs px-4 py-2 rounded-lg ${
-                message.sender === "User" ? "bg-custom text-white" : "bg-gray-300"
+                message.sender === "User" ? "bg-custom text-white" : "bg-slate-300"
               }`}
             >
-              {message.text}
+              <div className="flex items-center space-x-2">
+                <span>{message.text}</span>
+                {message.sender === "User" && (
+                  <Check className="text-slate-300" size={16} />
+                )}
+              </div>
+              <div className="text-xs text-slate-300 mt-1 text-right">{message.timestamp}</div>
             </div>
           </div>
         ))}
+        <div ref={messagesEndRef} />
       </div>
-      <div className="px-6 py-4 flex items-center space-x-2 bg-slate-50 border-t border-gray-300 rounded-t-3xl shadow-2xl">
+
+      <div className="px-6 py-4 flex items-center space-x-2 bg-slate-50 border-t border-t-slate-300 rounded-t-3xl shadow-2xl">
         <input
           value={input}
           onChange={(e) => setInput(e.target.value)}
