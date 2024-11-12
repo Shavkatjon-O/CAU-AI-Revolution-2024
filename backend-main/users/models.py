a809d9fb02093afcf6b08822bd8f438a0b6a86b0
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-12 15:28:50 +0500

    fix: change username field to model for user

--- backend-main/users/models.py
+++ backend-main/users/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ValidationError
 
 from users.managers import UserManager
 from common.models import BaseModel
 
 
 class User(AbstractUser, BaseModel):
-    email = None
-    first_name = None
-    last_name = None
+    username = None
+    # first_name = None
+    # last_name = None
+
+    email = models.EmailField(unique=True)
 
     REQUIRED_FIELDS = []
-    USERNAME_FIELD = "username"
+    USERNAME_FIELD = "email"
 
     objects = UserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
