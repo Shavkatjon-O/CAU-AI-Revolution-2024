bdfadaff4398d77c0a73a16bc8fb075a164fa08f
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-12 15:28:50 +0500

    fix

--- frontend-new/src/app/sign-in/page.tsx
+++ frontend-new/src/app/sign-in/page.tsx
@@ -3,8 +3,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-import { z } from "zod";
-
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -16,13 +14,17 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { signIn } from '@/services/auth';
+import { z } from "zod";
+import { useRouter } from "next/navigation";
 
 const FormSchema = z.object({
   email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
 });
 
 export function SignInForm() {
+  const router = useRouter();
+
   const [message, setMessage] = useState("");
 
   const form = useForm<z.infer<typeof FormSchema>>({
@@ -42,6 +44,8 @@ export function SignInForm() {
 
       if (response.success) {
         console.log("Sign-in successful");
+
+        router.push("/dashboard");
       } else {
         console.log("Sign-in failed");
       }
@@ -54,43 +58,60 @@ export function SignInForm() {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" placeholder="Email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <div className="size-full flex justify-center items-center">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-md w-full px-6 space-y-3">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Email" {...field}
+                    className="h-12"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="Password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Password" {...field}
+                    className="h-12"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <Button type="submit">Sign In</Button>
+          <Button
+            type="submit"
+            className="w-full h-12 bg-custom hover:bg-indigo-800 font-semibold"
+          >
+            Sign In
+          </Button>
 
-        {message && (
-          <span className="text-blue-500">{message}</span>
-        )}
-      </form>
-    </Form>
+          <div className="flex justify-center">
+            {message && (
+              <span className="text-indigo-500">{message}</span>
+            )}
+          </div>
+        </form>
+      </Form>
+    </div>
   );
 }
 
