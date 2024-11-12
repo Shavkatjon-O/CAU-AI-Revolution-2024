3d1f4a54910cd5d9a306907d21c0d21fca4443a3
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-11-12 15:28:50 +0500

    backend setup

---
+++ backend-ai/gunicorn_conf.py
@@ -0,0 +1,22 @@
+import os
+import multiprocessing
+
+wsgi_app = "core.wsgi:application"
+
+bind = f"0.0.0.0:8000"
+
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
+
+accesslog = "logs/backend/access.log"
+errorlog = "logs/backend/error.log"
+
+os.makedirs("logs/backend", exist_ok=True)
+
+if not os.path.exists(accesslog):
+    open(accesslog, "w").close()
+if not os.path.exists(errorlog):
+    open(errorlog, "w").close()
+
+
+timeout = 30
---
+++ backend-ai/users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+
+from users.managers import UserManager
+from common.models import BaseModel
+
+
+class User(AbstractUser, BaseModel):
+    email = None
+    first_name = None
+    last_name = None
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "username"
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
